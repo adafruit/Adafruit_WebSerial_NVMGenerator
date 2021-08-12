5ff088117b20a27fb87951b29d272321919be649
Melissa LeBlanc-Williams <melissa@adafruit.com>
2021-08-11 21:23:12 -0700

    fix debug=true

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
   var getParams = {}
   location.search.substr(1).split("&").forEach(function(item) {getParams[item.split("=")[0]] = item.split("=")[1]})
   if (getParams["debug"] !== undefined) {
-    debug = getParams["debug"] == "1" || getParams["debug"].toLowerCase() != "true";
+    debug = getParams["debug"] == "1" || getParams["debug"].toLowerCase() == "true";
   }
 
   espTool = new EspLoader({
